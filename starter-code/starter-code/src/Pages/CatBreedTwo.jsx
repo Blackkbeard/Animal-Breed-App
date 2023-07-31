5ee13b76addc7eecda008e3b9f89357b25b48ce5
Blackkbeard <rvinesh2011@gmail.com>
2023-07-31 18:55:13 +0800

    Dog api Fetch info

--- starter-code/starter-code/src/Pages/CatBreedTwo.jsx
+++ starter-code/starter-code/src/Pages/CatBreedTwo.jsx
@@ -2,10 +2,44 @@ import React from "react";
 import SingleCatOverlay from "./SingleCatOverlay";
 
 const CatBreedTwo = () => {
+  const [cats, setCats] = useState([]);
+  useEffect(() => {
+    const fetchCatData = async () => {
+      try {
+        const res = await fetch("https://api.thecatapi.com/v1/breeds");
+        const data = await res.json();
+        setCats(data);
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchCatData();
+  }, []);
+
   return (
-    <div>
-      <h2>Cat Breeds</h2>
-    </div>
+    <>
+      <div>
+        <div className="">Welcome to The dog breed info list</div>
+        <form>
+          <input
+            input="text"
+            name="search"
+            id="search"
+            placeholder="What breed you want?"
+          ></input>
+        </form>
+        <div>{}</div>
+      </div>
+      {/* <div className="containter">The dog Element</div>
+      <h2>Page One</h2>
+      <ul>
+        <Link to="/page-one/a">A</Link>
+      </ul>
+      <ul>
+        <Link to="/page-one/b">B</Link>
+      </ul> */}
+    </>
   );
 };
 
--- starter-code/starter-code/src/Pages/HomePageOne.jsx
+++ starter-code/starter-code/src/Pages/HomePageOne.jsx
@@ -1,19 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import SingleDogOverlay from "./SingleDogOverlay";
 
 const HomePageOne = () => {
+  const [dogs, setDogs] = useState([]);
+  useEffect(() => {
+    const fetchDogData = async () => {
+      try {
+        const res = await fetch("https://api.thedogapi.com/v1/breeds");
+        const data = await res.json();
+        setDogs(data);
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchDogData();
+  }, []);
   return (
     <>
-    <div className="containter">The dog Element</div>
+      <div>
+        <div className="">Welcome to The dog breed info list</div>
+        <form>
+          <input
+            input="text"
+            name="search"
+            id="search"
+            placeholder="What breed you want?"
+          ></input>
+        </form>
+        <div>
+          {dogs.map((dog) => (
+            <divdiv key={dog.id}></divdiv>
+          ))}
+        </div>
+      </div>
+      {/* <div className="containter">The dog Element</div>
       <h2>Page One</h2>
       <ul>
         <Link to="/page-one/a">A</Link>
       </ul>
       <ul>
         <Link to="/page-one/b">B</Link>
-      </ul>
-      
+      </ul> */}
     </>
   );
 };
