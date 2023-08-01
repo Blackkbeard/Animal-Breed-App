56b4d3fb509d26f5ef887377affc9f4cf85c928d
Blackkbeard <rvinesh2011@gmail.com>
2023-08-01 19:34:29 +0800

    Test

--- starter-code/starter-code/src/Navigate/NavBar.jsx
+++ starter-code/starter-code/src/Navigate/NavBar.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import styles from "./NavBar.module.css";
-import {  NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <header className={styles.navbar}>
       <nav>
         <ul>
-         
           <li>
             <NavLink
               className={(navData) => (navData.isActive ? styles.active : "")}
